ed9192fbeceb8a70d10660e772aad6c4cf93ce3e
Bougarra <bougarra.02@gmail.com>
2025-08-14 21:16:43 +0100

    Add Security coté Backend

--- ebanking-backend/src/main/java/ma/iibdcc/ebankingbackend/web/CustomerRestController.java
+++ ebanking-backend/src/main/java/ma/iibdcc/ebankingbackend/web/CustomerRestController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import ma.iibdcc.ebankingbackend.dtos.CustomerDTO;
 import ma.iibdcc.ebankingbackend.exceptions.CustomerNotFoundException;
 import ma.iibdcc.ebankingbackend.services.IBankAccountService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.PathMatcher;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,32 +18,38 @@ public class CustomerRestController {
     private IBankAccountService bankAccountService;
 
     @GetMapping("/customers")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public List<CustomerDTO> getAllCustomers() {
         return bankAccountService.listCustomers();
     }
 
     @GetMapping("/customers/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public CustomerDTO getCustomerById(@PathVariable(name = "id") Long customerId) throws CustomerNotFoundException {
         return bankAccountService.getCustomer(customerId);
     }
 
     @PostMapping("/customers")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO) {
         return bankAccountService.saveCustomer(customerDTO);
     }
 
     @PutMapping("/customers/{customerId}")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public CustomerDTO updateCustomer(@PathVariable Long customerId, @RequestBody CustomerDTO customerDTO) {
         customerDTO.setId(customerId);
         return bankAccountService.updateCustomer(customerDTO);
     }
 
     @DeleteMapping("/customers/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public void deleteCustomer(@PathVariable(name = "id") Long customerId) {
         bankAccountService.deleteCustomer(customerId);
     }
 
     @GetMapping("/customers/search")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public List<CustomerDTO> searchCustomer(@RequestParam(name = "keyword", defaultValue = "") String keyword) {
         return bankAccountService.searchCustomers("%"+keyword+"%");
     }
