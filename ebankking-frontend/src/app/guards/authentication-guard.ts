9558a393553ff9daf7b13a272bc1d26a81d515a5
Bougarra <bougarra.02@gmail.com>
2025-08-14 21:19:30 +0100

    Add Guard Authentication & Authorization

---
+++ ebankking-frontend/src/app/guards/authentication-guard.ts
@@ -0,0 +1,11 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth';
+import { inject } from '@angular/core';
+
+export const AuthenticationGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  if (authService.isAuthenticated == true) return true;
+  else router.navigateByUrl('/login');
+  return false;
+};
